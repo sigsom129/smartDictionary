d71122a52a8eb46a8fa8199ef1d8cc0e4284fb34
Sig Somsavath <ssomsavath@gmail.com>
2020-04-08 01:51:48 -0500

    Update

--- app1.py
+++ app1.py
@@ -9,6 +9,10 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word,data.keys())) > 0:
         res = input("Did you mean [%s]? Enter Y if yes or N if No \n"  % get_close_matches(word,data.keys())[0])
         if res == 'Y' or res == 'y':
